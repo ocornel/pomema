24ed1397e8b59d099cb261474320f3e235b21232
Martin Cornel <mrtncornel@gmail.com>
2021-01-18 17:00:00 +0300

    Landing page done.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,33 +65,42 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+{{--            @if (Route::has('login'))--}}
+{{--                <div class="top-right links">--}}
+{{--                    @auth--}}
+{{--                        <a href="{{ url('/home') }}">Home</a>--}}
+{{--                    @else--}}
+{{--                        <a href="{{ route('login') }}">Login</a>--}}
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+{{--                        @if (Route::has('register'))--}}
+{{--                            <a href="{{ route('register') }}">Register</a>--}}
+{{--                        @endif--}}
+{{--                    @endauth--}}
+{{--                </div>--}}
+{{--            @endif--}}
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+{{--                    Laravel--}}
+                    <img style="width: 50vw; margin: auto" src="{{ asset('img/logo_blue_white.png') }}" alt="{{ config('app.name', 'POMEMA logo') }}">
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @if (Route::has('login'))
+
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    <a href="{{ route('patients') }}">Patients</a>
+                    <a href="{{ route('noks') }}">Referees</a>
+                    <a href="{{ route('credits') }}">Credits</a>
+{{--                    <a href="https://blog.laravel.com">Blog</a>--}}
+{{--                    <a href="https://nova.laravel.com">Nova</a>--}}
+{{--                    <a href="https://forge.laravel.com">Forge</a>--}}
+{{--                    <a href="https://github.com/laravel/laravel">GitHub</a>--}}
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                    @endauth
+                        @endif
                 </div>
             </div>
         </div>
