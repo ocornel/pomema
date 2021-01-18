125fbaa1213afe35bb6bdd1da2eb8be93262bf3f
Martin Cornel <mrtncornel@gmail.com>
2021-01-18 19:00:00 +0300

    Edit patient.

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -25,7 +25,7 @@ class PatientController extends Controller
     {
         $patients = Patient::all();
         $context = [
-            'patients'=> $patients
+            'patients' => $patients
         ];
         return view('patient.index', $context);
     }
@@ -43,7 +43,7 @@ class PatientController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
      */
     public function store(Request $request)
@@ -51,26 +51,25 @@ class PatientController extends Controller
         $request['dob'] = date_create($request['dob']);
         $nok_id = $request['p_nok_id'];
         $patient = Patient::create($request->all());
-        $nok = NextOfKin::where('id_number',$nok_id)->first();
+        $nok = NextOfKin::where('id_number', $nok_id)->first();
         if ($nok) {
             PatientNok::create([
-                'patient_id'=>$patient->id,
+                'patient_id' => $patient->id,
                 'nok_id' => $nok->id,
                 'created_by' => $patient->created_by,
                 'is_primary' => true
             ]);
             Session::flash('success', 'Post created');
             return redirect(route('patients'));
-        }
-        else
+        } else
 //            redirect to creating nok with this id number
-        dd('Patient Stored, redirect to create NOK with id number '. $nok_id, $patient);
+            dd('Patient Stored, redirect to create NOK with id number ' . $nok_id, $patient);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Patient  $patient
+     * @param \App\Patient $patient
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
     public function show(Patient $patient)
@@ -84,30 +83,52 @@ class PatientController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Patient  $patient
-     * @return \Illuminate\Http\Response
+     * @param \App\Patient $patient
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
     public function edit(Patient $patient)
     {
-        dd('edit patient here', $patient);
+        $context = [
+            'patient' => $patient
+        ];
+        return view('patient.create', $context);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Patient  $patient
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Patient $patient
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
      */
     public function update(Request $request, Patient $patient)
     {
-        dd('update patient here', $patient, $request);
+        $request['dob'] = date_create($request['dob']);
+        $nok_id = $request['p_nok_id'];
+        $patient->update($request->all());
+        $nok = NextOfKin::where('id_number', $nok_id)->first();
+        if ($nok) {
+            foreach (PatientNok::where('patient_id', $patient->id)->get() as $pnok) {
+                $pnok->update(['is_primary' => false]);
+            }
+            PatientNok::updateOrCreate([
+                'patient_id' => $patient->id,
+                'nok_id' => $nok->id],
+                [
+                    'created_by' => $patient->created_by,
+                    'is_primary' => true
+                ]);
+            Session::flash('success', 'Post created');
+            return redirect(route('show_patient', [$patient, $patient->last_name]));
+        } else
+//            redirect to creating nok with this id number
+            dd('Patient Updated, redirect to create NOK with id number ' . $nok_id, $patient);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Patient  $patient
+     * @param \App\Patient $patient
      * @return \Illuminate\Http\Response
      */
     public function destroy(Patient $patient)
