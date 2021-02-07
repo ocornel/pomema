69129bb43225cb4e43dce96153d159e2419e488e
Martin Cornel <mrtncornel@gmail.com>
2021-02-07 20:21:01 +0300

    Post styling clean up.

--- resources/views/layouts/sidenav_old.blade.php
+++
@@ -1,7 +0,0 @@
-<ul>
-    <li><a href="{{ route('home') }}">Dashboard</a></li>
-    <li><a href="{{ route('patients') }}">Patients</a></li>
-    <li><a href="{{ route('credits') }}">Credits</a></li>
-    <li><a href="{{ route('noks') }}">Next of Kins</a></li>
-    <li><a href="{{ route('users') }}">Users</a></li>
-</ul>
