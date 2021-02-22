e67664ac3e807e98191716b7b5c7daf48c54325b
Martin Cornel <mrtncornel@gmail.com>
2021-02-22 18:00:00 +0300

    Removed server side changes.

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('/load_widget', 'HomeController@load_widget')->name('load_widget');
-
